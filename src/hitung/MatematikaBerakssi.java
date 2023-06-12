d88d28931cdcc6a50fab9e22bfd813c70bd4d6b5
devishafira <dvshafira@gmail.com>
2023-06-12 21:30:36 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBerakssi.java
+++ src/hitung/MatematikaBerakssi.java
@@ -3,5 +3,13 @@ package hitung;
 
 
 public class MatematikaBerakssi {
+     public static void main(String[] args) {
+    //membuat objek
+    Matematika devi = new Matematika (1,2);
     
+    System.out.println("Hasil penjumlahan : "+devi.setPenjumlahan());
+    System.out.println("Hasil pengurangan : "+devi.setPengurangan());
+    System.out.println("Hasil perkalian   : "+devi.setPerkalian());
+    System.out.println("Hasil pembagian   : "+devi.setPembagian());
+    } 
 }
